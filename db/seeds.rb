12a6853eed4aa2ce66c3c2e6399bbf5e90ae03f3
Gilad Manor <giladmanor01@gmail.com>
2014-01-03 23:30:03 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+account = Account.create({name:"Initial Test Account"})
+
+role_lock = [
+    {:name=>"Account", :items=>[{:name=>"Roles", :uri=>"/list/role"},{:name=>"Users", :uri=>"/list/user"}]},
+    {:name=>"Prospects", :items=>[{:name=>"Prospect", :uri=>"/list/prospect"}]}, 
+    
+  ]
+
+role = Role.create({account: account, name: "tech", code: "tech", lock: role_lock})
+User.create({ account: account, role: role,  login: "a", password: "a"})
