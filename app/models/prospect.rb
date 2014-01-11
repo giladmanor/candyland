66b5e912c7988e657413ddb380f4b01ed75189b6
Gilad Manor <giladmanor01@gmail.com>
2014-01-11 22:44:13 +0200

    compute score

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -3,11 +3,23 @@ class Prospect < ActiveRecord::Base
   belongs_to :user
   has_many :approches
   has_feed :approch,"actions"
-  to_info :name, :twitter,:likes, :assigned_to
+  to_info :name, :twitter,:likes, :assigned_to, :score
   
   
   def assigned_to
      self.user.nil? ? "" : self.user.login
+  end
+  
+  def score
+    sum = 0
+    self.approches.each{|a| 
+      sum+= 5 if a.retweeted
+      sum+= 3 if a.followed
+      sum+= 2 if a.replied
+      sum+= 1 if a.favorited
+      
+    }
+    sum
   end
   
 end
