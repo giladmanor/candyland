32a1be07358ce94b88a08a67a90b0c098466c38c
Gilad Manor <giladmanor01@gmail.com>
2014-01-20 23:02:50 +0200

    ulixes client

--- lib/ulixes.rb
+++ lib/ulixes.rb
@@ -4,18 +4,27 @@ require 'logger'
 #by Gilad Manor - a client module for Ulixes rule engine
 class Ulixes
   
-  def self register
-    
+  def self.register(uid)
+    send({:uid=>uid})
   end
   
-  def self.set
-    
+  def self.set(uid,code,value)
+    send({:uid=>uid, :code=>code, :value=>value})
   end
   
-  def self.get
-    
+  def self.get(uid)
+    send({:uid=>uid})
   end
   
+  def send(data)
+    open("http://#{@@server}?a_id=#{@@account_id}&k=#{@@account_key}&#{data.map{|k,v| "#{k}=#{v}"}.join("&")}")
+  end
+  
+  def self.credentials(server,id, key)
+    @@server = server
+    @@account_id = id
+    @@account_key = key
+  end
   
   def self.user_entity(e)
     @@entity = e
