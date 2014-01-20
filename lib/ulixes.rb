c5fb4e79d8a62277b6100ed1f2aa70335e7a4095
Gilad <giladmanor01@gmail.com>
2014-01-20 10:30:41 +0200

    Ulixes client init

---
+++ lib/ulixes.rb
@@ -0,0 +1,28 @@
+require "open-uri"
+require 'logger'
+
+#by Gilad Manor - a client module for Ulixes rule engine
+class Ulixes
+  
+  def self register
+    
+  end
+  
+  def self.set
+    
+  end
+  
+  def self.get
+    
+  end
+  
+  
+  def self.user_entity(e)
+    @@entity = e
+  end
+  
+  def self.uuid_field(f)
+    @@uuid_field = f
+  end
+  
+end
